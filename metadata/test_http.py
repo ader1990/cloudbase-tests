88d9e87da3674959a8b13f1d2a05a78c912ec346
Robert Tingirica <tingirica.robert@gmail.com>
2013-07-10 19:56:21 +0300

    working get_meta_data test

--- metadata/test_http.py
+++ metadata/test_http.py
@@ -19,93 +19,41 @@ import mox
 import unittest
 import urllib2
 
-#from cloudbaseinit.metadata.services import base
-from cloudbaseinit.openstack.common import cfg
-#from cloudbaseinit.test.metadata import fake
 from cloudbaseinit.metadata.services import httpservice
-#from cloudbaseinit.osutils import windows
-
-CONF = cfg.CONF
 
 
 class HttpServiceTest(unittest.TestCase):
     ''' testing http requests Class '''
 
     def setUp(self):
-
-        self._mox = mox.Mox()
+        self.mox = mox.Mox()
         self._setup_stubs()
 
     def _setup_stubs(self):
-        self._mox.StubOutWithMock(urllib2, 'Request')
-        self._mox.StubOutWithMock(urllib2, 'urlopen')
-        #self._mox.StubOutWithMock(windows.WindowsUtils, 'get_os_version')
-        #not yet self._mox.StubOutWithMock(posixpath, 'join')
+        self.mox.StubOutClassWithMocks(urllib2, 'Request')
+        self.mox.StubOutWithMock(urllib2, 'urlopen')
 
     def tearDown(self):
-        self._mox.UnsetStubs()
-        self._mox.ResetAll()
+        self.mox.UnsetStubs()
 
     def test_get_meta_data(self):
         svc = httpservice.HttpService()
+        response_mock = self.mox.CreateMockAnything()
 
-        fake_request = 'fake_request'
+        data_type = 'openstack'
         fake_meta_data = '{"fake_meta_data": "fake_value"}'
         version = 'latest'
-        data_type = 'openstack'
 
-        m = urllib2.Request(mox.IsA(str))
-        m.AndReturn(fake_request)
+        fake_request = urllib2.Request(mox.IsA(str))
 
         m = urllib2.urlopen(fake_request)
-        response_mock = mox.Mox().CreateMockAnything()
+        m.AndReturn(response_mock)
         m1 = response_mock.read()
         m1.AndReturn(fake_meta_data)
-        m.AndReturn(response_mock)
 
-        self._mox.ReplayAll()
+        self.mox.ReplayAll()
         meta_data = svc.get_meta_data(data_type, version)
-        self._mox.VerifyAll()
+        self.mox.VerifyAll()
 
         meta_data_compare = json.loads(fake_meta_data)
-        self.assertEquals(meta_data, meta_data_compare)
-
-
-
-
-
-'''
-    def test_post_password(self):
-        svc = httpservice.HttpService()
-
-        fake_request = 'fake_request'
-        fake_meta_data = '{"fake_meta_data": "fake_value"}'
-        version = 'latest'
-        data_type = 'openstack'
-        fake_data = 'fake_data'
-
-        m = urllib2.Request(mox.IsA(str))
-        m.AndReturn(fake_request)
-
-        m = urllib2.urlopen(fake_request, fake_data)
-        response_mock = mox.Mox().CreateMockAnything()
-        m1 = response_mock.read()
-        m1.AndReturn(fake_meta_data)
-        m.AndReturn(response_mock)
-
-        self._mox.ReplayAll()
-        meta_data = svc.get_meta_data(data_type, version)
-        self._mox.VerifyAll()
-
-        meta_data_compare = json.loads(fake_meta_data)
-        self.assertEquals(meta_data, meta_data_compare)
-
-        test_check_metadata_ip_route(self):
-            version = windows.WindowsUtils.get_os_version()
-            m.AndReturn(version)
-
-            self._mox.ReplayAll()
-            meta_data = svc._check_metadata_ip_route()
-            self._mox.VerifyAll()
-
-'''
+        self.assertEqual(meta_data, meta_data_compare)
